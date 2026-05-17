ee8830dff7f4b80df09f6034ce76fda950a660d8
Andy <andy@quietcode.org>
2026-05-17 10:07:55 +0200

    Add initial LeoCol Cocoa app scaffold

---
+++ App/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LeoCol
+//
+//  Created by Administrator on 17.05.26.
+//  Copyright __MyCompanyName__ 2026. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
